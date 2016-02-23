474449aabf3106824d5395fe82bc060fa5c7584f
Xing Wang <xingwang@cse.tamu.edu>
2016-02-23 16:46:39 -0600

    add more modules, building database, crawl information

--- code.py
+++ code.py
@@ -1,53 +1,64 @@
 import web, json
 
 urls=(
-	# pages
-	'/', 'index',
-	'/view_product', 'view_product',
-	'/list_product', 'list_product', 
-	'/compare', 'compare_products', # compare two or three products
+    # pages
+    '/', 'index',
+    '/view_product', 'view_product',
+    '/list_product', 'list_product', 
+    '/compare', 'compare_products', # compare two or three products
 
 
-	# API
-	'/search_product', 'search_product',
-	)
+    # API
+    '/search_product', 'search_product',
+    '/prod_info', 'prod_info',
+    )
 
 #############
 # HTML
 #############
 
 class index:
-	def GET(self):
-		render = web.template.render('templates/', base="base")
-		return render.index()
+    def GET(self):
+        render = web.template.render('templates/', base="base")
+        return render.index()
 
 
 class list_product:
-	def GET(self):
-		render = web.template.render('templates/', base="base")
-		return render.list_product()
+    def GET(self):
+        render = web.template.render('templates/', base="base")
+        return render.list_product()
 
 
 class view_product:
-	def GET(self):
-		render = web.template.render('templates/', base="base")
-		return render.view_product()
+    def GET(self):
+        render = web.template.render('templates/', base="base")
+        return render.view_product()
 
 class compare_products:
-	def GET(self):
-		render = web.template.render('templates/', base="base")
-		return render.compare_products()
+    def GET(self):
+        render = web.template.render('templates/', base="base")
+        return render.compare_products()
 
 
 #############
 # RESTFUL API
 #############
 
+class prod_info:
+    def GET(self):
+        from product_api import get_prod_info
+        res = get_prod_info()
+        res_dic = {'status':0, 'data':res}
+        return json.dumps(res_dic)
+
 class search_product:
-	def GET(self):
-		return json.dump({})
+    def GET(self):
+        from product_api import get_match_prod
+        tmp_res = get_match_prod()
+        res = {'status':0, 'data':tmp_res}
+        return json.dumps(res)
 
 
 if __name__ == "__main__":
-	app = web.application(urls, globals())
-	app.run()
+    app = web.application(urls, globals())
+    app.run()
--- product_api.py
+++ product_api.py
@@ -1,41 +1,99 @@
 import os
 import simplejson
 
+    
+
+
 def get_match_prod():
     tmp_res = [
 {
-'source':'Newegg',
-'id':'9SIA24G28N3012',
-'seller':'antonline-com',
-'price':148.2,
-'img':'http://images17.newegg.com/is/image/newegg/20-148-841-TS?$S640$',
-},
-{
-'source':'Newegg',
-'id':'N82E16820242104',
-'seller':'newegg',
-'price':133.4,
+'model':'CT4K4G4RFS8213',
+'brand':'Crucial',
+'imgs':[
+    'http://pisces.bbystatic.com/image2/BestBuy_US/images/mp/products/1312/1312976/1312976564/1312976564_1000x1000_sd.jpg;canvasHeight=500;canvasWidth=500',
+    'http://images17.newegg.com/is/image/newegg/20-148-841-TS?$S640$'],
+'rate':0,
+'unit':'4*4',
+
+'sellerlist':[
+    {
+        'source':'Amazon',
+        'id':'B00KUSMTW4',
+        'sellerid':'2529211011',
+        'sellername':'Crucial',
+        'price':153.03,
+        'left':'5'
+        },
+    {
+        'source':'Newegg',
+        'id':'9SIA24G28N3012',
+        'sellerid':'antonline-com',
+        'sellername':'antonline-com',
+        'price':148.2,
+        'left':'-1'
+        },
+    {
+        'source':'BestBuy',
+        'id':'1312976564',
+        'sellerid':'6006839520005',
+        'price':152.3,
+        'left':'-1'
+    }
+]
+
+
 },
 
 {
-'source':'Amazon',
-'id':'B00SK7NMD2',
-'seller':'8318979011',
-'price':29.95,
-'attention':'only 1 used',
-'img':'http://ecx.images-amazon.com/images/I/717CGJn1%2BhL._SX522_.jpg',
+'model':'KVR21R15S8K4/16',
+'brand':'Kingston',
+'rate':0,
+'imgs':[],
+'unit':'4*4',
+
+'sellerlist':[
+    {
+    'source':'Newegg',
+    'id':'N82E16820242104',
+    'sellerid':'newegg',
+    'price':133.4,
+    }, 
+    {
+        'source':'Amazon',
+        'id':'B00X60MU68',
+        'sellerid':'11351297011',
+        'sellername':'Kingston Technology',
+        'price':142.84,
+        }
+]
 },
 
 {
-'source':'BestBuy',
-'id':'1312976564',
-'seller':'6006839520005',
-'price':152.3,
-'img':'http://pisces.bbystatic.com/image2/BestBuy_US/images/mp/products/1312/1312976/1312976564/1312976564_1000x1000_sd.jpg;canvasHeight=500;canvasWidth=500'
-}
+'model':'HMA451R7MFR8N-TF',
+'brand':'hynix',
+'imgs':['http://ecx.images-amazon.com/images/I/717CGJn1%2BhL._SX522_.jpg'],
+'rate':0,
+'unit':'1*4',
+
+'sellerlist':[
+    {
+    'source':'Amazon',
+    'id':'B00SK7NMD2',
+    'sellerid':'8318979011',
+    'sellername':'GoldenRAM',
+    'price':29.95,
+    'left':1,
+    }
+]
+
+},
 
 ]
     return tmp_res
+
+def get_prod_info():
+    res = get_match_prod()
+    return res[0]
 
 def get_review():
     pass
